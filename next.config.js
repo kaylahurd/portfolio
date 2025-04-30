efbebc311632c5a9aec84366464498a4e9bcd803
Kayla Hurd <kaylahurd39@gmail.com>
2025-04-30 19:22:40 -0400

    Add ESLint config and update Next.js config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: 'standalone',
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig; 
+  reactStrictMode: true
+} 
