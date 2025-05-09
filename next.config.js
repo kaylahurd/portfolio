f0a3d9cfbb3506f61d0282475e6e495027eee030
Kayla Hurd <kaylahurd39@gmail.com>
2025-05-09 00:54:54 -0400

    Update Next.js config to disable all build checks

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    esmExternals: false,
   },
 }
 
