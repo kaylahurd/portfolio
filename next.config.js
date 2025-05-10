ee428af6ed962a379d663699d2ea3fb74cc9bb1f
Kayla Hurd <kaylahurd39@gmail.com>
2025-05-09 21:06:34 -0400

    fix: simplify Next.js config to resolve build issues

--- next.config.js
+++ next.config.js
@@ -4,29 +4,10 @@ const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
-    dirs: ['app', 'components', 'pages']
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  experimental: {
-    esmExternals: false,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      use: [
-        {
-          loader: 'eslint-loader',
-          options: {
-            emitWarning: true,
-            failOnError: false,
-          },
-        },
-      ],
-    });
-    return config;
-  },
+  }
 }
 
 module.exports = nextConfig 
